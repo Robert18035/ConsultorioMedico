77dcd4a8af6844d493ae829b830d48843fac79a5
Robert18035 <betometeoeo@live.com.mx>
2019-05-30 11:15:12 -0500

    Parte 1 de validacion de formulario con AngularJS - Agregar validaciones faltantes

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -1,3 +1,4 @@
+/* ----------------------------------- ROUTING -----------------------------------*/
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
@@ -10,7 +11,40 @@ app.config(function($routeProvider) {
         .when("/registro", {
             templateUrl: "../views/registro.html"
         })
+        .when("/altaRegistro", {
+            templateUrl: "../views/altaRegistro.html"
+        })
         .otherwise({
             templateUrl: "../views/error.html"
         });
+});
+
+/* ----------------------------------- VALIDAR FORMULARIO -----------------------------------*/
+
+
+app.controller('pacienteController', function($scope) {
+    $scope.persons = {};
+    $scope.edad = 0;
+    $scope.sexo = '';
+    $scope.register = function() {
+        $scope.nameInvalid = false;
+        $scope.emailInvalid = false;
+        $scope.sexInvalid = false;
+        $scope.ageInvalid = false;
+        if (!$scope.registrationForm.nombre.$valid) {
+            $scope.nameInvalid = true;
+        }
+        if (!$scope.registrationForm.email.$valid) {
+            $scope.emailInvalid = true;
+        }
+        if (!$scope.registrationForm.sexo.$valid) {
+            $scope.emailInvalid = true;
+        }
+        if (!$scope.registrationForm.edad.$valid) {
+            $scope.emailInvalid = true;
+        }
+        if ($scope.registrationForm.$valid) {
+            alert("Registro exitoso");
+        }
+    }
 });

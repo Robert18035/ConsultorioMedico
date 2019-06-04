9eed27ab961b8281b8f22f63754a359a55b84533
Robert18035 <betometeoeo@live.com.mx>
2019-06-03 20:15:45 -0500

    Agregados los formularios, me falta validar la contraseña del Médico, pero todo lo demás ya sirve, si quieres ir agregando tus archivos adelante

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -3,7 +3,8 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "../views/principal.html"
+            templateUrl: "../views/principal.html",
+            controller: "homeController"
         })
         .when("/about", {
             templateUrl: "../views/about.html"
@@ -15,27 +16,67 @@ app.config(function($routeProvider) {
             templateUrl: "../views/altaRegistro.html",
             controller: "registroPac"
         })
+        .when("/altaRegistro-success", {
+            templateUrl: "../views/principal.html",
+            controller: "registroPac"
+        })
         .when("/medico", {
             templateUrl: "../views/registroMedico.html",
+            controller: "registroMed"
+        })
+        .when("/medico-success", {
+            templateUrl: "../views/principal.html",
             controller: "registroMed"
         })
         .when("/enfermera", {
             templateUrl: "../views/registroEnfermera.html",
             controller: "registroEnf"
         })
+        .when("/enfermera-success", {
+            templateUrl: "../views/principal.html",
+            controller: "registroEnf"
+        })
         .otherwise({
             templateUrl: "../views/error.html"
         });
 });
+/* NUNCA BORRAR ESTE CONTROLADOR */
+app.controller('homeController', function($scope) {
+    $scope.message = 'Realiza tu consulta online con nosotros';
+});
 
 /* -------------------------------- FORMULARIO ALTA REGISTRO -------------------------------*/
 
-app.controller('registroPac', function($scope) {
+app.controller('registroPac', ['$scope', '$location', function($scope, $location) {
+    $scope.paciente = [];
+    $scope.paciente.sexo = [
+        { value: "Masculino", label: "Masculino" },
+        { value: "Femenino", label: "Femenino" }
+    ]
     $scope.registrar = function() {
-        var nombre = $scope.paciente.nombre;
-        var email = $scope.paciente.email;
-        $scope.paciente = {};
-        console.log(nombre);
-        console.log(email);
+        $location.path('/altaRegistro-success');
+    };
+}]);
+/* -------------------------------- FORMULARIO REGISTRO MÉDICO -------------------------------*/
+
+app.controller('registroMed', ['$scope', '$location', function($scope, $location) {
+    $scope.medico = [];
+    var pass1 = $scope.medico.pass;
+    var pass2 = $scope.medico.pass2;
+    /* Falta implementar bien esto */
+    if (pass1 != pass2) {
+        alert("Contraseñas no coinciden");
+    } else {
+        $scope.registrarMed = function() {
+            $location.path('/medico-success');
+        };
     }
-});
+}]);
+
+/* ------------------------------ FORMULARIO REGISTRO ENFERMERA -----------------------------*/
+app.controller('registroEnf', ['$scope', '$location', function($scope, $location) {
+    $scope.enfermera = [];
+    $scope.registrarEnf = function() {
+        $location.path('/enfermera-success');
+    };
+}]);
